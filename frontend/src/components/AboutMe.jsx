8890ddf11dc45391c5e2f1083b5a66add22aefb6
Chien-Hwa Yang <123.ysheep@gmail.com>
2024-05-17 21:38:07 +1000

    add navbar and main page structure

---
+++ frontend/src/components/AboutMe.jsx
@@ -0,0 +1,45 @@
+import Avatar from '@mui/material/Avatar';
+import Chip from '@mui/material/Chip';
+import Stack from '@mui/material/Stack';
+
+const AboutMe = () => {
+
+    const handleClick = () => {
+        console.info('You clicked the Chip.');
+      };
+
+    return (
+        <>
+        <div style={{display: 'flex', width:'60%', height: '60%', margin: 'auto'}}>
+            <div style={{backgroundColor: '#a5a5a5', width: '40%'}}>
+                <img alt='dp' src='../dp.jpg' style={{borderRadius: '50%', width: '100%', border: '8px solid #ddd'}}/>
+            </div>
+            <div style={{backgroundColor: '#efefef', padding: '2%', width: '60%'}}>
+                <h6>UNSW Student</h6>
+                <h2>Iris Yang</h2>
+                <p>
+                    Hello ! I'm a Computer Science bachelor's graduate from Taiwan currently pursuing a Master of Information Technology with a focus on Database System, expected to complete my degree by May 2025.
+                    Experienced in website building(HTML/CSS & JavaScript), database design(SQL, Python), text-to-speech model application(PyTorch).
+                    Seeking an frontend/full-stack engineering internship to enhance my capabilities. I am eager to contribute my skills and learn from experienced professionals in a dynamic and collaborative environment.
+                </p>
+                <Stack direction="row" spacing={1}>
+                    <Chip
+                        avatar={<Avatar alt="GitHub" src="github-icon.png" />}
+                        label="GitHub"
+                        variant="outlined"
+                        onClick={handleClick}
+                    />
+                    <Chip
+                        avatar={<Avatar alt="LinkedIn" src="linkedin-icon.webp" />}
+                        label="LinkedIn"
+                        onClick={handleClick}
+                    />
+                </Stack>
+            </div>
+        </div>
+        
+        </>
+    )
+}
+
+export default AboutMe;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,57 @@
+import { useState } from 'react';
+import { CNavbar, CNavbarBrand, CNavbarToggler, COffcanvas, COffcanvasHeader, COffcanvasTitle, CCloseButton, COffcanvasBody, CNavbarNav, CNavItem, CNavLink, CDropdown, CDropdownToggle, CDropdownMenu, CDropdownItem, CDropdownDivider, CContainer } from '@coreui/react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Navbar.css';
+
+const Navbar = () => {
+    const [visible, setVisible] = useState(false)
+    return (
+    <CNavbar className="bg-body-tertiary" expand="sm">
+        <CContainer fluid>
+        <CNavbarBrand>IrisYang</CNavbarBrand>
+
+        <CNavbarToggler
+            aria-controls="offcanvasNavbar2"
+            aria-label="Toggle navigation"
+            onClick={() => setVisible(!visible)}
+        />
+
+        <COffcanvas id="offcanvasNavbar2" placement="end" portal={false} visible={visible} onHide={() => setVisible(false)}>
+            <COffcanvasHeader>
+                <COffcanvasTitle>
+                        IrisYang
+                </COffcanvasTitle>
+                <CCloseButton className="text-reset" onClick={() => setVisible(false)} />
+            </COffcanvasHeader>
+            <COffcanvasBody>
+
+            <CNavbarNav>
+                <CNavItem>
+                    <CNavLink href="#" active>
+                        About Me
+                    </CNavLink>
+                </CNavItem>
+                <CNavItem>
+                    <CNavLink href="#">Resume</CNavLink>
+                </CNavItem>
+                <CDropdown variant="nav-item" popper={false}>
+                    <CDropdownToggle color="secondary">Projects</CDropdownToggle>
+                    <CDropdownMenu>
+                        <CDropdownItem href="#">Action</CDropdownItem>
+                        <CDropdownItem href="#">Another action</CDropdownItem>
+                        <CDropdownDivider />
+                        <CDropdownItem href="#">Something else here</CDropdownItem>
+                    </CDropdownMenu>
+                </CDropdown>
+                
+            </CNavbarNav>
+            
+            </COffcanvasBody>
+        </COffcanvas>
+        </CContainer>
+    </CNavbar>
+    )
+
+}
+
+export default Navbar;
