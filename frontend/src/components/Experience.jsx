3180d7bcda2c2181daf7356ee6b97328d15f6468
Chien-Hwa Yang <123.ysheep@gmail.com>
2024-06-16 10:12:39 +1000

    indent

--- frontend/src/components/Experience.jsx
+++ frontend/src/components/Experience.jsx
@@ -1,81 +1,61 @@
-import Chip from '@mui/material/Chip';
-import Stack from '@mui/material/Stack';
-import './Experience.css';
+import Chip from "@mui/material/Chip";
+import Stack from "@mui/material/Stack";
+import "./Experience.css";
 
 const Experience = () => {
-    
-    return (
-        <>
-        <h3>Experience</h3>
-            <div className='text-section'>
-                <h5>AnyParking</h5>
-                <p>
-                The Car Space Renting Management System connects parking suppliers and customers.
-                Providers can offer spots and establish fees, while customers can browse, reserve, and pay for parking.
-                The system provides security through user roles (admin, supplier, and consumer) and contains capabili- ties for billing, payment, and feedback.
-                This platform intends to maximise parking space use, improve urban mobility, and alleviate parking stress.
-                </p>
-                <Stack direction="row" spacing={1}>
-                    <Chip
-                        label="React"
-                        variant="outlined"
-                    />
-                    <Chip
-                        label="Django"
-                        variant="outlined"
-                    />
-                    <Chip
-                        label="Python"
-                        variant="outlined"
-                    />
-                    <Chip
-                        label="SQLite"
-                        variant="outlined"
-                    />
-                    <Chip
-                        label="Postman"
-                        variant="outlined"
-                    />
-                </Stack>
-            </div>
+  return (
+    <>
+      <h3>Experience</h3>
+      <div className="exp-section">
+        <div className="exp-left">
+          <h5>Feb 2022 - Oct 2022</h5>
+        </div>
+        <div className="exp-right">
+          <h5>Linebot Developer</h5>
+          <h5>CHANG GUNG UNIVERSITY OF SCIENCE AND TECHNOLOGY, Taiwan</h5>
+          <p>
+            Developed Linebots using the LINE API SDKs, including profile page,
+            patient list, Scale form, handbook of dementia care, viewing
+            patient&#39;s record and exporting patient&#39;s history report in
+            Excel format.
+          </p>
+          <Stack
+            direction="row"
+            spacing={{ xs: 1, sm: 1 }}
+            flexWrap="wrap"
+            useFlexGap
+          >
+            <Chip label="Python" variant="outlined" />
+            <Chip label="Amazon EC2" variant="outlined" />
+            <Chip label="Line" variant="outlined" />
+          </Stack>
+        </div>
+      </div>
 
-            <div className='text-section'>
-                <h5>LmTraceMap</h5>
-                <p>
-                A global tracking website specifically designed for experts to monitor the spread of L.monocytogenes,
-                a challenging bacterium to track due to its presence in food shipments worldwide.
-                The platform aims to facilitate comprehensive monitoring and data sharing,
-                enabling experts from different regions to collaborate effectively in tracking L.monocytogenes outbreaks.
-                </p>
-                <Stack direction="row" spacing={1}>
-                    <Chip
-                        label="VanillaJS"
-                        variant="outlined"
-                    />
-                    <Chip
-                        label="Python"
-                        variant="outlined"
-                    />
-                    <Chip
-                        label="php"
-                        variant="outlined"
-                    />
-                    <Chip
-                        label="HTML"
-                        variant="outlined"
-                    />
-                    <Chip
-                        label="CSS"
-                        variant="outlined"
-                    />
-                    <Chip
-                        label="MySQL"
-                        variant="outlined"
-                    />
-                </Stack>
-            </div>
-        </>
-    )
-}
+      <div className="exp-section">
+        <div className="exp-left">
+          <h5>Sep 2021 - Sep 2022</h5>
+        </div>
+        <div className="exp-right">
+          <h5>LmTraceMap</h5>
+          <p>
+            Tutored 30+ students in learning Python from scratch, providing
+            guidance on fundamental concepts and empowered them to extract
+            information from websites they are interested in.
+          </p>
+          <Stack
+            direction="row"
+            spacing={{ xs: 1, sm: 1 }}
+            flexWrap="wrap"
+            useFlexGap
+          >
+            <Chip label="Python" variant="outlined" />
+            <Chip label="BeautifulSoup" variant="outlined" />
+          </Stack>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Experience;
