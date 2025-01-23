ffa98a35a4ef257cc8ff5c642be0a37c95a7cd83
Chien-Hwa Yang <123.ysheep@gmail.com>
2025-01-24 10:22:08 +1100

    update about me content

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,36 @@
 const About = () => {
-    return (
-        <>
-        <h2>About</h2>
-        <p>
-            Hello ! I'm a Computer Science bachelor's graduate from Taiwan currently pursuing a Master of Information Technology with a focus on Database System in UNSW, expected to complete my degree by May 2025.
-            Experienced in website building(HTML/CSS & JavaScript), database design(SQL, Python), text-to-speech model application(PyTorch).
-            Seeking an frontend/full-stack engineering internship to enhance my capabilities. I am eager to contribute my skills and learn from experienced professionals in a dynamic and collaborative environment.
+  return (
+    <>
+      <h2>About</h2>
+      <div>
+        <p className="mb-4">
+          I'm a developer who enjoys using technology to solve practical issues
+          and has an inquisitive mind. I have a strong desire to create
+          solutions that are not only useful but also simple to use and
+          pleasurable. I believe that careful design and strong engineering come
+          together to create magic.
         </p>
-        </>
-    )
-}
+
+        <p className="mb-4">
+          At UNSW, I'm presently pursuing a Master of Information Technology
+          with an emphasis on software development, database systems, and
+          cybersecurity. I work at PICA Group in addition to my studies, where I
+          automate operations and streamline procedures to support digital
+          transformation.
+        </p>
+
+        <p className="mb-4">
+          In my spare time, I'm usually walking, singing, playing the guitar,
+          and taking pictures. These are the things that inspire my creativity
+          and help me stay grounded.
+        </p>
+
+        <p>
+        Let’s connect — I’m always open to collaborating on exciting projects or swapping ideas about the latest in tech!
+        </p>
+      </div>
+    </>
+  );
+};
 
 export default About;
